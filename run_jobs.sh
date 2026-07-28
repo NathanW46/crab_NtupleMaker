4aa83fd0a76a4cf63323eac26b9aa42f8665cdbe
Nathan Whittington <nathan.whittington@cern.ch>
2026-07-28 23:33:46 +0200

    new jobs

--- run_jobs.sh
+++ run_jobs.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/bash
-python3 submit_crab.py -n 9000 -t 4 -s SUSY0PU -algo HYBRID_DISPLACED
-python3 submit_crab.py -n 9000 -t 4 -s SUSY200PU -algo HYBRID_DISPLACED
-python3 submit_crab.py -n 9000 -t 4 -s SUSY0PU -algo HYBRID_SIM
-python3 submit_crab.py -n 9000 -t 4 -s SUSY200PU -algo HYBRID_NEWKF
+python3 submit_crab.py -n 10000 -t 8 -s DMuGun200PU -algo HYBRID_DISPLACED
+python3 submit_crab.py -n 10000 -t 8 -s DMuGun200PU -algo HYBRID_SIM_DISPLACED
+python3 submit_crab.py -n 1000 -t 8 -s SUSY200PU -algo HYBRID_DISPLACED
+python3 submit_crab.py -n 1000 -t 8 -s SUSY200PU -algo HYBRID_SIM_DISPLACED
 
 
 
